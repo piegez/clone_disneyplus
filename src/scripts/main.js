7af2dc46f25f18f825f7b1a6963137912a8e0830
Jessica Sampaio <jessicamsampaio@gmail.com>
2024-03-08 19:12:01 -0300

    cofig uglify

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight
 
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
 
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
+    // seção de atrações e programação das abas 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -14,10 +26,21 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
+    //faq
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
 })
+
+function ocultaElementosHeader () {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader () {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 function abreOuFechaResposta(elemento){
     const classe = 'faq__questions__item--is-open';
